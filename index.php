71867c05a41b20a1b0f7da0cec74e7d495a68518
PLCook <edhky1@gmail.com>
2015-07-06 08:59:58 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+
+
+	    <meta charset="UTF-8"> 
+        <meta name="description" content="Paul L. Cook">
+        <meta name="keywords" content="Paul Cook, Web Developer, Launchcode, HTML">
+        <meta name="author" content="Paul Cook">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    	<title>Paul L. Cook</title>
+	</head>
+	<body>
+		<div align="center">
+		<header>
+			<div class="navbar-wrapper navbar-default navbar-fixed-top" role="navigation"> 
+				<div class="container"> 
+					<div class="navbar-header"> 
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"> 
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Paul L. Cook</a>
+					</div>
+					<div id="navbar" class="collapse navbar-collapse"> 
+						<ul class="nav navbar-nav navbar-right"> 
+							<li class="active"><a href="index.php">Home</a></li>
+							<li><a href="about.html">About Me</a></li>
+							<li><a href="resume.php">My Resume</a></li>
+							<li><a href="references.php">My References</a></li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="container-fluid">
+        	<h1>Paul L. Cook</h1>
+        	</div>
+		</header>
+		<main>
+			<article>
+				<section>
+					<p>Hi. If you are on my personal page, I can only guess that you've seen my personalized email address and wanted to see what a website looks like when someone owns his or her own domain. Sorry to disappoint.<br>I generally do not use this site as anything but a placeholder for the email address, so it's never been very fancy. In fact, until recently, it hadn't been updated since 2005.</p>
+				</section>
+			</article>
+		</main>
+	</div>
+	</body>
+			<script src="http://code.jquery.com/jquery.js"></script>
+		    <script language="javascript" type="text/javascript" src="js/bootstrap.js"></script>
+	    	<script language="javascript" type="text/javascript" src="js/bootstrap.min.js"></script>
+</html>
